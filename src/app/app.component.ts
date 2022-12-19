388dba2c43067af0072f93228be4d069ee5be7c6
Karthik Raj <raj@channelassist.com>
2022-12-19 21:56:40 +0530

    latest code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+
 import { DataService } from './data.service';
 
+interface MartketData {
+  high: number,
+  low: number,
+  open: number,
+  UB: number,
+  LB: number,
+  orderOpen: boolean,
+  orderSide: string,
+  target: number,
+  stopLoss: number,
+  priceToTrade: number,
+  trades: [],
+  status: number,
+  isFirstTrade: boolean,
+  comments: string[]
+}
 enum Val {
   open = 1,
   high = 2,
   low = 3,
+}
+enum Order {
+  nill = 1,
+  pendingBUY = 2,
+  pendingSell = 3,
+  liveBuy = 4,
+  liveSell = 5,
+  completed = 6,
 }
 @Component({
   selector: 'app-root',
@@ -13,8 +38,8 @@ enum Val {
 })
 export class AppComponent implements OnInit {
   title = 'Kanna-statergyAngular';
-  marketObject: { high: number, low: number, open: number, UB: number | null, LB: number | null, leaseUB: number, leaseLB: number } = {
-    high: 0, low: 0, open: 0, UB: null, LB: null, leaseUB: 0, leaseLB: 0
+  marketObject: MartketData = {
+    high: 0, low: 0, open: 0, UB: 0, LB: 0, orderOpen: false, orderSide: '', target: 0, stopLoss: 0, priceToTrade: 0, trades: [], status: Order.nill, isFirstTrade: true,comments:[]
   };
   trades = [];
   constructor(private datas: DataService) { }
@@ -31,13 +56,80 @@ export class AppComponent implements OnInit {
         return;
       }
 
-      if (data[Val.high] > this.marketObject.high) {
+      // if(this.marketObject.orderOpen){
+      //   if(this.marketObject.orderSide == 'BUY'){
+      //     if(this.marketObject.orderType == 'TGT'){
 
-        this.setUpperBandAndLowerBand(data, "NEWHIGH")
+      //     }else if(this.marketObject.orderType == 'Normal'){
+
+      //     }
+      //   }else if(this.marketObject.orderSide == "SELL"){
+      //     if(this.marketObject.orderType == 'TGT'){
+      //       if(data[Val.low] <= this.marketObject.priceToTrade){
+      //         let order = {side: 'SELL',type: 'TGT', sellPrice:this.marketObject.priceToTrade,stopLoss: this.marketObject.stopLoss,
+      //                       target: this.marketObject.target}
+      //         this.marketObject.orderOpen = false;              
+      //       }
+      //     }else if(this.marketObject.orderType == 'Normal'){
+
+      //     }
+      //   }
+      // }
+
+      switch (this.marketObject.status) {
+        case 1: {
+          this.checkToPlaceOrder(data);
+          break;
+        }
+        case 2: {
+          if (this.marketObject.target == 0) {
+            //Normal Buy goes here
+
+            if (data[Val.high] >= this.marketObject.priceToTrade) {
+              //Trade executed logic goes here
+              if (data[Val.high] > this.marketObject.high) {
+                this.marketObject.high = data[Val.high];
+              }
+              this.marketObject.status = Order.liveBuy;
+              this.marketObject.isFirstTrade = false;
+              this.setUpperBandAndLowerBand(data)
+              return
+            }
+
+            if (data[Val.low] <= this.marketObject.LB) {
+              //Cancel Buy order and place Sell order
+              this.marketObject.comments.push(`LB Buy cancelled`)
+              this.checkToPlaceOrder(data);
+              // this.marketObject.priceToTrade = this.marketObject.low - 1;
+              // this.marketObject.orderSide = 'SELL';
+              // this.marketObject.status = Order.pendingSell;
+            }
+
+            //cancel Buy order 
+            // new Low or Low crossed existing lower band
+
+
+          } else {
+            //TGT BUY logic goes here
+          }
+          //normal order 
+          // Order execute 
+          // New High OR New low Order may cancel and repeat case 1 logic
+          //TGT Order
+          // Order execute
+          // May reach stoploss and repeat case 1 logic  
+          break;
+        }
+        case 3: {
+          //normal order 
+          // update new UB and LB and check Track SL 
+          // SL May hit and repeat case 1 by reset high,low,UB,LB
+          //TGT Order
+          // TGT reached and trake new UB ,LB,High,LOw
+          // May reach SL and repeat case 1  
+        }
       }
-      if (data[Val.low] < this.marketObject.low) {
-        this.setUpperBandAndLowerBand(data, "NEWLOW")
-      }
+
       // if(data[Val.high] >= )
       // console.log(index);
     })
@@ -45,31 +137,70 @@ export class AppComponent implements OnInit {
   }
 
 
-  //Find upper and Lower Band
-  setUpperBandAndLowerBand(data: any, type?: string) {
-    debugger
-    if (type == "NEWHIGH") {
-      this.marketObject.high = data[Val.high];
-    }
-    if (type == "NEWLOW") {
-      this.marketObject.low = data[Val.low];
-    }
-    let diff = data[Val.high] - data[Val.low];
-    this.marketObject.UB = this.customParseFloat(data[Val.high] + diff);
-    this.marketObject.LB = this.customParseFloat(data[Val.low] - diff);
 
-    if (this.marketObject.leaseLB == 0 && this.marketObject.leaseUB == 0) {
-      this.marketObject.leaseLB = this.marketObject.LB;
-      this.marketObject.leaseUB = this.marketObject.UB;
+
+  //Check to place Order
+  checkToPlaceOrder(data: any) {
+    //Initial order excution
+    if (data[Val.high] >= this.marketObject.high) {
+      this.marketObject.high = data[Val.high];
+      this.setUpperBandAndLowerBand(data)
     }
-    //200 < 0
-    if (this.marketObject.UB < this.marketObject.leaseUB) {
-      console.log("Hi")
-      this.marketObject.leaseUB = this.marketObject.UB;
+    if (data[Val.high] >= this.marketObject.UB) {
+      this.marketObject.orderOpen = true;
+      if (data[Val.high] < this.marketObject.high) {
+        this.setTargetFunction('BUY')
+      }
+      this.marketObject.orderSide = "BUY";
+      this.marketObject.status = Order.pendingBUY
+      this.marketObject.priceToTrade = this.customParseFloat(data[Val.high] + 1);
+      if(this.marketObject.target) this.marketObject.comments.push(`Crossed UB Buy Order placed at ${data[Val.high] + 1} Target at ${this.marketObject.target}, Stoploss:${this.marketObject.stopLoss}`);
+      else this.marketObject.comments.push(`Crossed UB Buy Order placed at ${data[Val.high] + 1}`)
+      console.log(`Crossed UB Buy Order placed at ${data[Val.high] + 1}`);
+      return
     }
-    //18468 > 18501 
-    if (this.marketObject.leaseLB > this.marketObject.LB) {
-      this.marketObject.leaseLB = this.marketObject.LB;
+    if (data[Val.low] <= this.marketObject.low) {
+      this.marketObject.low = data[Val.low];
+      this.setUpperBandAndLowerBand(data)
+    }
+    if (data[Val.low] <= this.marketObject.LB) {
+      this.marketObject.orderOpen = true;
+      if (data[Val.low] > this.marketObject.low) {
+        this.setTargetFunction('SELL')
+      }
+      this.marketObject.orderSide = "SELL"
+      this.marketObject.priceToTrade = this.customParseFloat(data[Val.low] - 1);
+      this.marketObject.status = Order.pendingSell
+      console.log(`Crossed LB SELL Order placed at ${data[Val.low] - 1}`);
+      return
+    }
+  }
+  //set Target function
+  setTargetFunction(type: string) {
+    if (type == "BUY") {
+      let dif: number = this.marketObject.open - this.marketObject.low;
+      this.marketObject.target = this.marketObject.open + dif;
+      this.marketObject.stopLoss = this.marketObject.low;
+    } else if (type == "SELL") {
+      let dif: number = this.marketObject.high - this.marketObject.open;
+      this.marketObject.target = this.marketObject.open - dif;
+      this.marketObject.stopLoss = this.marketObject.high;
+    }
+
+  }
+
+  //Find upper and Lower Band
+  setUpperBandAndLowerBand(data: any) {
+    let diff = data[Val.high] - data[Val.low];
+    let UB = this.customParseFloat(data[Val.high] + diff);
+    let LB = this.customParseFloat(data[Val.low] - diff);
+    if (this.marketObject.isFirstTrade) {
+      if (!this.marketObject.UB || this.marketObject.UB >= UB) this.marketObject.UB = UB;
+      //18264 < 18275
+      if (!this.marketObject.LB || LB >= this.marketObject.LB) this.marketObject.LB = LB;
+    } else {
+      this.marketObject.UB = UB;
+      this.marketObject.LB = LB;
     }
 
   }
