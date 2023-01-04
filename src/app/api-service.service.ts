a8d526cfec2a7c4f40e22f9dcaacc35269da71fb
Karthik Raj <raj@channelassist.com>
2023-01-04 15:35:04 +0530

    bug fixed

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders,} from '@angular/common/http';
+import {environment} from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
-
+  hostUrl: string = environment.url;
+  uri: string = '/getCandles';
   constructor(public httpClient: HttpClient,) { }
 
   getEvery5MinsData(date: string,token: string){
-    return this.httpClient.get(`http://localhost:3000?date=${date}&token=${token}`)
+    return this.httpClient.post(`${this.hostUrl}${this.uri}`,{date,token})
   }
 }
